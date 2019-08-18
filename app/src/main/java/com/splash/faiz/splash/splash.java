f326c54880ec8294697d23aa607fa56acf9f91eb
Banee Ishaque K <baneeishaque@gmail.com>
2019-08-18 17:50:11 +0530

    l

--- app/src/main/java/com/splash/faiz/splash/splash.java
+++ app/src/main/java/com/splash/faiz/splash/splash.java
@@ -10,30 +10,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class splash extends AppCompatActivity {
-    private TextView tv ;
+    private TextView tv;
     private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        tv = (TextView) findViewById(R.id.tv) ;
-        iv =(ImageView) findViewById(R.id.iv) ;
-        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition) ;
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
         tv.startAnimation(myanim);
         iv.startAnimation(myanim);
-        final Intent i = new Intent(this,MainActivity.class);
-        Thread timer =new Thread() {
-            public void run () {
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
                 try {
                     sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
-                finally {
+                } finally {
                     startActivity(i);
                     finish();
-
                 }
             }
         };
